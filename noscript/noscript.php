716052f4f4f1315cc6526f583b997cf6da33ce27
Ash Matadeen <ash.matadeen@gmail.com>
2015-08-16 16:47:09 +0100

    fix for noscript - position text area so it shows just below the recaptcha images

--- noscript/noscript.php
+++ noscript/noscript.php
@@ -1,5 +1,5 @@
 <noscript>
-  <div style="width: 302px; height: 425px; margin-bottom: 20px; margin-left: -15px;">
+  <div style="width: 302px; height: 495px; margin-bottom: 20px; margin-left: -15px;">
     <div style="width: 302px; height: 425px; position: relative;">
       <div style="width: 302px; height: 425px; position: absolute;">
         <iframe src="https://www.google.com/recaptcha/api/fallback?k=<?php echo get_option( 'wr_no_captcha_site_key' ); ?>"
@@ -8,13 +8,12 @@
         </iframe>
       </div>
       <div style="width: 300px; height: 60px; border-style: none;
-                  bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px;
-                  background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;">
+                  bottom: 12px; left: 1px; margin: 0px; padding: 0px; right: 1px;
+                  background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px; position: absolute; top: 435px;">
         <textarea id="g-recaptcha-response" name="g-recaptcha-response"
                   class="g-recaptcha-response"
                   style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
-                         margin: 10px 25px; padding: 0px; resize: none;" >
-        </textarea>
+                         margin: 10px 25px; padding: 0px; resize: none;" ></textarea>
       </div>
     </div>
   </div>
